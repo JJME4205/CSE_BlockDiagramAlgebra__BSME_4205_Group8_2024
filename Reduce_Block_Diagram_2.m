6cd2b600bb63ec1ad9dd7f0286e3b4adac41a4a9
Janellaandal <159086810+Janellaandal@users.noreply.github.com>
2024-05-21 02:46:25 -0700

    Update Reduce_Block_Diagram_2.m

--- Reduce_Block_Diagram_2.m
+++ Reduce_Block_Diagram_2.m
@@ -4,4 +4,13 @@ clear
 clc
 close all
 
+%% Define G1,G2 and H
+% Part of Janella Mae D. Andal 20-06308
+G1_num = [1];
+G2_den = [1 0 0];
 
+G2_num = [1];
+G2_den = [1 1];
+
+G3_num = [1];
+G3_den = [1 0];
